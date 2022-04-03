fa78587c847a302420f067f2c6b9e54f1ce02ec3
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2022-04-02 22:47:11 -0400

    update Netlify links.

--- server.ts
+++ server.ts
@@ -38,17 +38,17 @@ mongoose.connect(connectionString);
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: process.env.CORS_ORIGIN
+    origin: "https://resonant-capybara-859716.netlify.app"
 }));
 
 let sess = {
     secret: process.env.EXPRESS_SESSION_SECRET,
     saveUninitialized: true,
     resave: true,
-    // cookie: {
-    //     sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
-    //     secure: process.env.NODE_ENV === "production",
-    // }
+    cookie: {
+        sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
+        secure: process.env.NODE_ENV === "production",
+    }
 }
 
 if (process.env.NODE_ENV === 'production') {
