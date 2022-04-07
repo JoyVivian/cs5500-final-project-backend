2d8072277e66b191a6e4096e9ab830699e4250a8
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2022-04-06 22:30:32 -0400

    change the mongoose database

--- server.ts
+++ server.ts
@@ -29,7 +29,7 @@ require("dotenv").config();
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.dgng5.mongodb.net";
+const HOST = "cluster0.pt2jx.mongodb.net";
 const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
--- services/AuthenticationService2.ts
+++ services/AuthenticationService2.ts
@@ -6,7 +6,7 @@ const userDao: UserDao = UserDao.getInstance();
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.dgng5.mongodb.net";
+const HOST = "cluster0.pt2jx.mongodb.net";
 const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
